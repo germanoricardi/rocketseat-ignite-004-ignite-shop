a7c60ce8cb41586e071acd2d5106f25a89549923
Germano Ricardi <germanoricardi7@gmail.com>
2023-03-18 21:19:04 -0400

    Implementado Link do next para navegar até a página de detalhes do produto;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import 'keen-slider/keen-slider.min.css'
 import { stripe } from "@/lib/stripe";
 import { GetStaticProps } from "next";
 import Stripe from "stripe";
+import Link from "next/link";
 
 interface HomeProps {
   products: {
@@ -30,13 +31,15 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {
         products.map(product => (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} alt={product.name} width={520} height={480} />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link key={product.id} href={`/product/${product.id}`}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} alt={product.name} width={520} height={480} />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         ))
       }
     </HomeContainer>
