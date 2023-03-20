5afbb81233e3b2a674d96d162119b3ca76c2fd6e
Germano Ricardi <germanoricardi7@gmail.com>
2023-03-20 12:33:17 -0400

    Formatação css da página de produto;

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,21 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter()
-  return <h1>Product! {JSON.stringify(query)}</h1>
+  
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione amet facilis ullam unde temporibus, illum numquam, vitae rem ipsa veritatis sit quod consectetur! Voluptates culpa, libero nesciunt minus consequuntur corporis.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
