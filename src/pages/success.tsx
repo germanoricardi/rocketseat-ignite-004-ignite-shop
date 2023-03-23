4842b6be6c242b0d22a4191fa2a96b007e4221d1
Germano Ricardi <germanoricardi7@gmail.com>
2023-03-23 13:20:30 -0400

    Criada estrutura da página de sucesso;

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,18 @@
+import { ImageContainer } from "@/styles/pages/success";
+import { SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
-  return <h1>Success!</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+      </ImageContainer>
+
+      <p>Uhuul <strong>Diego Fernandes</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa. </p>
+
+      <Link href={'/'}>Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
